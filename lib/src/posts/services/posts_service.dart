3efdf8241d5860609a95bbb96f145a4e3706b055
Adeilson Silva <adeilson@gmail.com>
2022-01-07 19:33:16 -0300

    - uno http client - post model - fetch all posts

---
+++ lib/src/posts/services/posts_service.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_value_notifier/src/posts/models/post_model.dart';
+import 'package:uno/uno.dart';
+
+class PostsService {
+  final Uno uno;
+
+  PostsService(this.uno);
+
+  Future<List<PostModel>> fetchPosts() async {
+    final response =
+        await uno.get('https://jsonplaceholder.typicode.com/posts');
+    final list = response.data as List;
+    final posts = list.map((item) => PostModel.fromMap(item)).toList();
+
+    return posts;
+  }
+}
