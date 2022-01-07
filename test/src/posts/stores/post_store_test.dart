15379f4258953ebb2a2f8e1ef4ac322996a5d61b
Adeilson Silva <adeilson@gmail.com>
2022-01-07 20:17:41 -0300

    test all state changes

--- test/src/posts/stores/post_store_test.dart
+++ test/src/posts/stores/post_store_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_value_notifier/src/posts/services/posts_service.dart';
 import 'package:flutter_value_notifier/src/posts/states/post_state.dart';
 import 'package:flutter_value_notifier/src/posts/stores/post_store.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:value_listenable_test/value_listenable_test.dart';
 
 class PostServiceMock extends Mock implements PostService {}
 
@@ -11,15 +12,31 @@ void main() {
   final store = PostStore(service);
   test('should be change alter state to success', () async {
     when(() => service.fetchPosts()).thenAnswer((_) async => []);
-    await store.fetchProducts();
 
-    expect(store.value, isA<SuccessPostState>());
+    expect(
+        store,
+        emitValues(
+          [
+            isA<LoadingPostState>(),
+            isA<SuccessPostState>(),
+          ],
+        ));
+
+    await store.fetchProducts();
   });
 
   test('should be change alter state to error', () async {
     when(() => service.fetchPosts()).thenThrow(Exception('Error'));
-    await store.fetchProducts();
 
-    expect(store.value, isA<ErrorPostState>());
+    expect(
+        store,
+        emitValues(
+          [
+            isA<LoadingPostState>(),
+            isA<ErrorPostState>(),
+          ],
+        ));
+
+    await store.fetchProducts();
   });
 }
